fc895a24019fb03e6bec9c51d39ebf5a7846ec00
Akshita Chotaliya <107063411+AkshitaChotaliya@users.noreply.github.com>
2025-06-23 22:27:07 +0530

    worked on middleware functionality for auth api

--- myapp/middleware.py
+++ myapp/middleware.py
@@ -1,14 +1,21 @@
 from django.http import JsonResponse
+
 class APILoggingMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.path.startswith('/api/auth') or request.path in [
+
+        open_paths = [
             '/signup/', '/login/', '/logout/', '/password-reset/',
             '/password-reset/confirm/', '/webhook/', '/register/'
-        ]:
+        ]
+
+        if request.path.startswith('/api/auth') or request.path in open_paths:
             print(f"[API LOG] {request.method} {request.path}")
+        else:
+            if not request.user.is_authenticated:
+                return JsonResponse({'detail': 'Authentication required.'}, status=401)
 
         response = self.get_response(request)
         return response
